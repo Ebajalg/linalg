3774c0b7e8ce0e8ceb32e94259b77e47684a8955
Ebajalg <emmottmarkus@gmail.com>
2021-12-29 21:57:18 +0000

    Added reverse methods for mul, sub and add and integrated Polynomial objects to work in Matrix objects

--- polynomial.py
+++ polynomial.py
@@ -19,14 +19,20 @@ class Polynomial:
     def __mul__(self, other):
         output_p = Polynomial("")
         output_p.p_cs = {}
-        for i in self.p_cs.keys():
-            for j in other.p_cs.keys():
-                if (i + j) in output_p.p_cs.keys():
-                    output_p.p_cs[i + j] = output_p.p_cs[i + j] + self.p_cs[i]*other.p_cs[j]
-                else:
-                    output_p.p_cs[i + j] = self.p_cs[i]*other.p_cs[j]
-        output_p.powers = output_p.p_cs.keys()
-        output_p.constants = output_p.p_cs.values()
+        if isinstance(other, Polynomial):
+            for i in self.p_cs.keys():
+                for j in other.p_cs.keys():
+                    if (i + j) in output_p.p_cs.keys():
+                        output_p.p_cs[i + j] = output_p.p_cs[i + j] + self.p_cs[i]*other.p_cs[j]
+                    else:
+                        output_p.p_cs[i + j] = self.p_cs[i]*other.p_cs[j]
+        elif isinstance(other, float) or isinstance(other, int):
+            output_p = self * Polynomial(str(other))
+        else:
+            error_message = f"Type {type(other)} is not supported for multiplication with a polynominal object"
+            raise TypeError(error_message)
+        output_p.powers = list(output_p.p_cs.keys())
+        output_p.constants = list(output_p.p_cs.values())
         return output_p
 
     def __add__(self, other):
@@ -34,12 +40,17 @@ class Polynomial:
         if isinstance(other, Polynomial):
             output_p.powers = list(set(self.powers + other.powers))
             output_p.p_cs = {i: self.p_cs.get(i, 0) + other.p_cs.get(i, 0) for i in output_p.powers}
-            output_p.constants = output_p.p_cs.values()
         elif isinstance(other, float) or isinstance(other, int):
-            output_p = self - Polynomial(str(other))
+            output_p = self + Polynomial(str(other))
         else:
             error_message = f"Type {type(other)} is not supported for addition with a polynominal object"
             raise TypeError(error_message)
+        for i in list(output_p.p_cs.keys()).copy():
+            print(i)
+            if output_p.p_cs[i] == float(0):
+                del output_p.p_cs[i]
+                output_p.powers.remove(i)
+        output_p.constants = output_p.p_cs.values()
         return output_p
 
     def __sub__(self, other):
@@ -53,7 +64,23 @@ class Polynomial:
         else:
             error_message = f"Type {type(other)} is not supported for subtraction with a polynominal object"
             raise TypeError(error_message)
+        for i in list(output_p.p_cs.keys()).copy():
+            print(i)
+            if output_p.p_cs[i] == float(0):
+                del output_p.p_cs[i]
+                output_p.powers.remove(i)
+        output_p.constants = output_p.p_cs.values()
         return output_p
+
+    __rmul__ = __mul__
+    __radd__ = __add__
+
+    def __rsub__(self, other):
+        print(self)
+        print(other)
+        return Polynomial(str(other)) - self
+
+    # TODO: Add functionality for __rsub__
 
     def f(self, x):
         return sum(self.p_cs[i]*(x**i) for i in self.p_cs)
