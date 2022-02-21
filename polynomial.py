d6e7fa3b6581b7a0097547fc57e5a2ef24f5443e
Markus Emmott <emmottmarkus@gmail.com>
2022-02-21 01:38:00 +0000

    Changed show method to __str__

--- polynomial.py
+++ polynomial.py
@@ -1,9 +1,8 @@
-
 class Polynomial:
     def __init__(self, equation):
         # TODO: Add error messages for invalid equation inputs
         equation_terms = list(filter(''.__ne__, equation.replace('+', ',').replace('-', ',-').split(',')))
-        self.powers = [float(term[term.index('x')+2:len(term)]) if ('^' in term)
+        self.powers = [int(term[term.index('x') + 2:len(term)]) if ('^' in term)
                        else (1 if ('x' in term)
                              else 0)
                        for term in equation_terms]
@@ -13,7 +12,7 @@ class Polynomial:
                           else (float(term) if (term != '') else 0)
                           for term in equation_terms]
 
-        self.p_cs = {a:b for a,b in zip(self.powers, self.constants)}
+        self.p_cs = {a: b for a, b in zip(self.powers, self.constants)}
 
     # TODO: Add functionality for multiple types of other
     def __mul__(self, other):
@@ -23,9 +22,9 @@ class Polynomial:
             for i in self.p_cs.keys():
                 for j in other.p_cs.keys():
                     if (i + j) in output_p.p_cs.keys():
-                        output_p.p_cs[i + j] = output_p.p_cs[i + j] + self.p_cs[i]*other.p_cs[j]
+                        output_p.p_cs[i + j] = output_p.p_cs[i + j] + self.p_cs[i] * other.p_cs[j]
                     else:
-                        output_p.p_cs[i + j] = self.p_cs[i]*other.p_cs[j]
+                        output_p.p_cs[i + j] = self.p_cs[i] * other.p_cs[j]
         elif isinstance(other, float) or isinstance(other, int):
             output_p = self * Polynomial(str(other))
         else:
@@ -46,7 +45,6 @@ class Polynomial:
             error_message = f"Type {type(other)} is not supported for addition with a polynominal object"
             raise TypeError(error_message)
         for i in list(output_p.p_cs.keys()).copy():
-            print(i)
             if output_p.p_cs[i] == float(0):
                 del output_p.p_cs[i]
                 output_p.powers.remove(i)
@@ -65,7 +63,6 @@ class Polynomial:
             error_message = f"Type {type(other)} is not supported for subtraction with a polynominal object"
             raise TypeError(error_message)
         for i in list(output_p.p_cs.keys()).copy():
-            print(i)
             if output_p.p_cs[i] == float(0):
                 del output_p.p_cs[i]
                 output_p.powers.remove(i)
@@ -76,16 +73,14 @@ class Polynomial:
     __radd__ = __add__
 
     def __rsub__(self, other):
-        print(self)
-        print(other)
         return Polynomial(str(other)) - self
 
     # TODO: Add functionality for __rsub__
 
     def f(self, x):
-        return sum(self.p_cs[i]*(x**i) for i in self.p_cs)
+        return sum(self.p_cs[i] * (x ** i) for i in self.p_cs)
 
-    def show(self):
-        print('+'.join([f"{self.p_cs[i]}x^{i}"
-                        for i in self.p_cs.keys()]
-                       ).replace('+-', '-').replace('x^0', ''))
+    def __str__(self):
+        return (' + '.join([f"{self.p_cs[i]}x^{i}"
+                          for i in self.p_cs.keys()]
+                         ).replace('+ -', '- ').replace('x^0', ''))
